a264419e6c1dabd34efc5f7eeccc2ebe00be6970
HBilal Khan Yousafzai <hbilal_9@hotmail.com>
2024-03-15 17:59:18 +0500

    BroadcastServiceProvider middleware and prefix setup

--- app/Providers/BroadcastServiceProvider.php
+++ app/Providers/BroadcastServiceProvider.php
@@ -12,7 +12,10 @@ class BroadcastServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        Broadcast::routes();
+        Broadcast::routes([
+            'prefix' => 'api',
+            'middleware' => 'auth:sanctum'
+        ]);
 
         require base_path('routes/channels.php');
     }
